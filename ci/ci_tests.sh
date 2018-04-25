828925c42d3280fa337bab3ebbd9e1d8e9ccfe25
Seth Shelnutt <Shelnutt2@gmail.com>
2018-04-25 06:40:17 -0400

    Update mariadb version. #1

--- ci/ci_tests.sh
+++ ci/ci_tests.sh
@@ -4,10 +4,10 @@ set -e -x
 mkdir tmp
 shopt -s extglob
 mv !(tmp) tmp # Move everything but tmp
-wget https://downloads.mariadb.org/interstitial/mariadb-10.2.10/source/mariadb-10.2.10.tar.gz \
-&& tar xf mariadb-10.2.10.tar.gz \
-&& mv tmp mariadb-10.2.10/storage/mytile \
-&& cd mariadb-10.2.10 \
+wget https://downloads.mariadb.org/interstitial/mariadb-10.2.14/source/mariadb-10.2.14.tar.gz \
+&& tar xf mariadb-10.2.14.tar.gz \
+&& mv tmp mariadb-10.2.14/storage/mytile \
+&& cd mariadb-10.2.14 \
 && mkdir build \
 && cd build \
 && cmake -GNinja -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug .. \
