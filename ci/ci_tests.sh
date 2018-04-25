94f38944836f4dd1a0595206f328086b1b4ff7d0
Seth Shelnutt <Shelnutt2@gmail.com>
2018-04-25 06:44:02 -0400

    Switch mariadb building from ninja to make, ninja is broken in 10.2.14. #1

--- ci/ci_tests.sh
+++ ci/ci_tests.sh
@@ -10,7 +10,7 @@ wget https://downloads.mariadb.org/interstitial/mariadb-10.2.14/source/mariadb-1
 && cd mariadb-10.2.14 \
 && mkdir build \
 && cd build \
-&& cmake -GNinja -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug .. \
-&& ninja \
+&& cmake -DPLUGIN_TOKUDB=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_SPHINX=NO -DPLUGIN_FEDERATED=NO -DPLUGIN_FEDERATEDX=NO -DPLUGIN_CONNECT=NO -DCMAKE_BUILD_TYPE=Debug .. \
+&& make -j$(nproc) \
 && if ! ./mysql-test/mysql-test-run.pl  --suite=mytile --debug; then cat ./mysql-test/var/log/mysqld.1.err && false; fi;
 set +e +x
